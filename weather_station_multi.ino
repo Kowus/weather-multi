07bc4217a9d9e65268422f8b9ab782c816e03b33
kowus <barnabas@elite-education.org>
2017-05-31 08:21:02 +0000

    config wind speed computation

--- weather_station_multi.ino
+++ weather_station_multi.ino
@@ -12,11 +12,19 @@ const byte REFERENCE_3V3 = A3;
 const byte LIGHT = A1;
 const byte BATT = A2;
 const byte WDIR = A0;
+const byte RAIN = 2;
+const byte WSPEED = 3;
 const long interval = 1000;
-const long postInterval = 20000;
+const long postInterval = 120000;
 
 unsigned long previousMillis = 0;
 unsigned long postMillis = 0;
+volatile unsigned long rainlast = 0;
+volatile unsigned long lastWindIRQ = 0;
+volatile unsigned long lastWindCheck = 0;
+volatile float dailyrainin = 0, thisrainin = 0;
+volatile int wind_clicks = 0;
+
 float lightRead = 0, speedRead = 0, pressRead = 0, humRead = 0, tempRead = 0;
 int dirRead = 0, iterations = 0;
 
@@ -34,24 +42,29 @@ File myFile;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Initializing Weather Shield");
+  Serial.print("Initializing Weather Shield:\t");
   pinMode(LIGHT, INPUT);
   pinMode(SS, OUTPUT);
   pinMode(fileLed, OUTPUT);
-
+  pinMode(RAIN, INPUT_PULLUP);
+  pinMode(WSPEED, INPUT_PULLUP
+  
+  attachInterrupt(0, rainIRQ, FALLING);
+  attachInterrupt(1, wspeedIRQ, FALLING);
+  
   myPressure.begin();
   myPressure.setModeBarometer();
   myPressure.setOversampleRate(7);
   myPressure.enableEventFlags();
 
   myHumidity.begin();
-  Serial.println("\n\nWeather Shield Initialized\n\nInitializing SD Card");
+  Serial.print("success!\n\nInitializing SD Card:\t");
 
   if (!SD.begin(chipSelect)) {
-    Serial.println("initialization failed!");
+    Serial.println("failed!");
     return;
   }
-  Serial.println("\n\nInitialization done.");
+  Serial.println("success!");
 
 }
 
@@ -76,47 +89,46 @@ void loop() {
       myPressure.enableEventFlags();
       myHumidity.begin();
     } else {
-      /*
-
-
-
-
-
-        Serial.println("Writing to File");
-        Serial.println(output);
-        digitalWrite(fileLed, HIGH);
-        myFile = SD.open("hive00.log", FILE_WRITE);
-        if (myFile) {
-        myFile.println(output);
-        myFile.close();
-        Serial.println("Success");
-
-        } else {
-        Serial.println("error opening log file.");
-        }*/
-
+      //      Cummulative addition of readings per turn
       lightRead += get_light_level();
       humRead += humidity;
       dirRead += get_wind_direction();
       pressRead += myPressure.readPressure();
       tempRead += myHumidity.readTemperature();
       digitalWrite(fileLed, LOW);
+      //      Number of turns
       iterations += 1;
-      Serial.println("Step:\t"+String(iterations));
+      Serial.println("Step:\t" + String(iterations));
+      //      Reset timer
       previousMillis = currentMillis;
     }
   } else if (currentMillis - postMillis >= postInterval) {
     char output[300] = "";
     root.set<float>("light", float(lightRead) / iterations); // Light
     root.set<int>("wind_dir", dirRead / iterations); // Wind Direction
-//    root.set<float>("wind_spd", 0.00);  // Wind Speed
-    root.set<float>("pressure", float(pressRead)/iterations);    // pressure
-    root.set<float>("temperature", float(tempRead)/iterations);      // temperature
-//    root.set<float>("rain", 0.00);      // rainfall
-    root.set<float>("humidity", float(humRead)/iterations);      // humidity
+    //    root.set<float>("wind_spd", 0.00);  // Wind Speed
+    root.set<float>("pressure", float(pressRead) / iterations);  // pressure
+    root.set<float>("temperature", float(tempRead) / iterations);    // temperature
+    root.set<float>("totalrain", float(dailyrainin));      // total rainfall
+    root.set<float>("lastrain", float(thisrainin));      // rainfall
+    root.set<float>("humidity", float(humRead) / iterations);    // humidity
+    root.set<float>("battery", get_battery_level());
     root.printTo(output);
-        Serial.println();
-    lightRead = humRead = dirRead = pressRead = tempRead = iterations = 0;
+    Serial.println(output);
+    
+    Serial.print("write_to_file:\t");
+    digitalWrite(fileLed, HIGH);
+    myFile = SD.open("hive00.log", FILE_WRITE);
+    if (myFile) {
+      myFile.println(output);
+      myFile.close();
+      Serial.println("success!");
+
+    } else {
+      Serial.println("error!");
+    }
+    //    Reset values
+    lightRead = humRead = dirRead = pressRead = tempRead = iterations =thisrainin= 0;
     postMillis = currentMillis;
   }
 
@@ -167,3 +179,52 @@ int get_wind_direction()
   else if (adc < 990) return (270);
   else return (-1); // error, disconnected?
 }
+
+float get_battery_level()
+{
+  float operatingVoltage = analogRead(REFERENCE_3V3);
+
+  float rawVoltage = analogRead(BATT);
+
+  operatingVoltage = 3.30 / operatingVoltage; //The reference voltage is 3.3V
+
+  rawVoltage = operatingVoltage * rawVoltage; //Convert the 0 to 1023 int to actual voltage on BATT pin
+
+  rawVoltage *= 4.90; //(3.9k+1k)/1k - multiple BATT voltage by the voltage divider to get actual system voltage
+
+  return (rawVoltage);
+}
+
+
+
+
+
+
+
+// Interrupt Handlers
+
+void rainIRQ()
+// Count rain gauge bucket tips as they occur
+// Activated by the magnet and reed switch in the rain gauge, attached to input D2
+{
+  volatile unsigned long raintime = millis(); // grab current time
+  if (raintime - rainlast > 10) // ignore switch-bounce glitches less than 10mS after initial edge
+  {
+    dailyrainin += 0.011; //Each dump is 0.011" of water
+    thisrainin += 0.011; //Increase this minute's amount of rain
+
+    rainlast = raintime; // set up for next event
+  }
+}
+
+
+void wspeedIRQ()
+// Activated by the magnet in the anemometer (2 ticks per rotation), attached to input D3
+{
+  if (millis() - lastWindIRQ > 10) // Ignore switch-bounce glitches less than 10ms (142MPH max reading) after the reed switch closes
+  {
+    lastWindIRQ = millis(); //Grab the current time
+    windClicks++; //There is 1.492MPH for each click per second.
+  }
+}
+
